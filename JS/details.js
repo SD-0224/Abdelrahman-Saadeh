51a945a2b7324f6afe58ea409630d3bc22e25ae7
Abdelrahman <abdelrahman.hasan0@gmail.com>
2024-03-09 14:59:03 +0200

    JS second push

---
+++ JS/details.js
@@ -0,0 +1,87 @@
+async function fetchDetails(){
+  try{
+    const urlParam = new URLSearchParams(window.location.search);
+    const cardID = urlParam.get('id');
+    console.log(cardID);
+    const response = await fetch(`https://tap-web-1.herokuapp.com/topics/details/${cardID}`);
+    if (!response){
+      throw new Error('request failed');
+    }
+    const cardDetails = await response.json();
+    console.log('response is' , cardDetails);
+
+    const detailsMain = document.getElementById('details-main');
+    detailsMain.innerHTML = `
+    <div class="details-main-content">
+        <section class="details-descrip-container">
+          <div class="details-descrip">
+            <div>
+              <p class="details-course-name">${cardDetails.category}</p>
+              <p class="details-course-title">${cardDetails.topic}</p>
+              <div class="rating-stars">
+                <span><ion-icon class="star" name="star"></ion-icon></span>
+                <span><ion-icon class="star" name="star"></ion-icon></span>
+                <span><ion-icon class="star" name="star"></ion-icon></span>
+                <span><ion-icon class="star" name="star"></ion-icon></span>
+                <span><ion-icon class="star" name="star-outline"></ion-icon></span>
+              </div>
+            </div>
+            <p class="details-course-descrip">
+              ${cardDetails.description}
+            </p>
+          </div>
+      
+
+        </section>
+
+        <section class="html-topics-container">
+          <div class="top-topic">
+            <p class="html-topics-title">${cardDetails.topic} Sub Topics</p>
+          </div>
+          <div>
+            <p><span><ion-icon class="details-icon" name="checkmark-circle-outline"></ion-icon></span>
+              ${cardDetails.subtopics[0]}</p>
+          </div>
+          <div>
+            <p><span><ion-icon class="details-icon" name="checkmark-circle-outline"></ion-icon></span>
+            ${cardDetails.subtopics[1]}</p>
+          </div>
+          <div>
+            <p><span><ion-icon class="details-icon" name="checkmark-circle-outline"></ion-icon></span>
+            ${cardDetails.subtopics[2]}</p>
+          </div>
+          <div>
+            <p><span><ion-icon class="details-icon" name="checkmark-circle-outline"></ion-icon></span>
+            ${cardDetails.subtopics[3]}</p>
+          </div>
+          <div>
+            <p><span><ion-icon class="details-icon" name="checkmark-circle-outline"></ion-icon></span>
+            ${cardDetails.subtopics[4]}</p>
+          </div>
+          <div class="bottom-topic">
+            <p><span><ion-icon class="details-icon" name="checkmark-circle-outline"></ion-icon></span>
+            ${cardDetails.subtopics[5]}</p>
+          </div>
+
+        </section>
+      </div>
+
+      <section class="details-side-card">
+        <img src="Logos/${cardDetails.image}" class="logo-card">
+        <div class="card-title"><p>${cardDetails.topic}</strong> by <a href="">${cardDetails.name}</a></p></div>
+        <div class="favorites-box">
+          <label class="button-label">Interested about this topic?</label><br>
+          <button class="favorites-button">
+            Add to Favorites <span><ion-icon class="heart-card" name="heart-outline"></ion-icon></span>
+          </button>
+          <p class="card-footer">Unlimted Credits</p>
+        </div>
+      </section>
+    `
+  }
+
+  catch(error){
+    console.error('error fetching details', error);
+  }
+}
+fetchDetails();
--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,75 @@
 const apiInfo='https://tap-web-1.herokuapp.com/topics/list'
 console.log("hello");
-async function fetchCards(){
+let allCards = [];
+async function fetchCards(apiUrl=apiInfo){
   try{
-    const response = await fetch(apiInfo);
+    const response = await fetch(apiUrl);
     if(!response.ok){
       throw new Error('Fetch request failed');
     }
-    const cards = await response.json();
-    console.log('response is' ,cards );
+    allCards = await response.json();
+    console.log('response is' ,allCards );
     
+    displaycards(allCards);
     
+}
+catch(error){
+  console.error('error fetching data' , error);
+}
 
-    const topicsFound = document.getElementById('topics-found')
+}
+
+let debounceTime;
+async function searchResult(){
+  const searchInput = document.getElementById('search-bar');
+  const searchWord = searchInput.value.trim().toLowerCase();
+
+  clearTimeout(debounceTime);
+  debounceTime = setTimeout(async ()=>{
+    const searchAPI = `https://tap-web-1.herokuapp.com/topics/list?phrase=${searchWord}`;
+    await fetchCards(searchAPI);
+  }, 300);
+}
+
+function sortAndFilter (sortOption , filterOption){
+  let finalCards=[...allCards];
+
+  if(filterOption !== 'default'){
+    const filterOptionLower = filterOption.toLowerCase();
+    finalCards = finalCards.filter(card => card.category.toLowerCase() === filterOptionLower);
+
+  }
+
+  if(sortOption === 'Topic Title'){
+    finalCards.sort((a,b) => a.topic.localeCompare(b.topic));
+  } else if(sortOption ==='Author Name'){
+    finalCards.sort((a,b) => a.name.localeCompare(b.name));
+  }
+displaycards(finalCards);
+}
+
+const searchEntry = document.getElementById('search-bar');
+searchEntry.addEventListener('input' ,searchResult);
+const filterDropdown = document.getElementById('filter-by');
+filterDropdown.addEventListener('change', ()=>{
+  const filterOption= filterDropdown.value;
+  const sortOption=document.getElementById('sort-by').value;
+  sortAndFilter(sortOption,filterOption);
+
+});
+const sortDropdown = document.getElementById('sort-by');
+sortDropdown.addEventListener('change', sortAndFilter);
+
+
+fetchCards();
+
+function displaycards(cards){
+  const topicsFound = document.getElementById('topics-found')
     topicsFound.innerHTML = '';
 
-    cards.forEach(card => {
+    cards.forEach(card => {  
+    const cardLink = document.createElement('a');
+    cardLink.href =`details.html?id=${card.id}`;
     const courseCard = document.createElement('div');
     courseCard.classList.add('course-card');
     
@@ -55,13 +110,9 @@ for(let i=0; i < 4 ; i++){
    cardInfo.appendChild(courseAuthor);
 
    courseCard.appendChild(cardInfo);
+   cardLink.appendChild(courseCard);
+   topicsFound.appendChild(cardLink);
+   
 
 });
 }
-catch(error){
-  console.error('error fetching data' , error);
-}
-
-}
-
-fetchCards();
